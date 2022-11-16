9f884bf4fdf21864e18485e7a32c8fd90cf85d05
Kevw94 <pinerokevin@icloud.com>
2022-11-16 12:23:38 +0100

    implement connection for mongo + add .env for variables + add config for using it

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_ecurie/screens/home_page.dart';
 
-void main() {
+import 'providers/mongodb.dart';
+
+var mongodb = DBConnection.getInstance();
+void main() async {
+  // LOAD DOTENV 
+  await dotenv.load(fileName: ".env");
+  // CONNECT MONGO 
+  await mongodb.connect();
   runApp(const MyApp());
 }
 
