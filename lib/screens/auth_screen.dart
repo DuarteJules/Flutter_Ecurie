cebe7d03a1252dd119428fd9387627f7a8fcb62b
Kevw94 <pinerokevin@icloud.com>
2022-11-16 19:16:29 +0100

    remove unsused print

--- lib/screens/auth_screen.dart
+++ lib/screens/auth_screen.dart
@@ -11,7 +11,6 @@ class AuthScreen extends StatefulWidget {
 }
 
 class _AuthScreenState extends State<AuthScreen> {
-  bool isSwitched = false;
   int login = 0;
 
   @override
@@ -28,7 +27,6 @@ class _AuthScreenState extends State<AuthScreen> {
                 setState(() {
                   login = 0;
                 });
-                print(isSwitched);
               },
               style: ElevatedButton.styleFrom(shape: const StadiumBorder()),
               child: const Text('login'),
@@ -38,7 +36,6 @@ class _AuthScreenState extends State<AuthScreen> {
                 setState(() {
                   login = 1;
                 });
-                print(isSwitched);
               },
               style: ElevatedButton.styleFrom(shape: const StadiumBorder()),
               child: const Text('signup'),
---
+++ lib/widgets/week_coming.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+
+class WeekComing extends StatefulWidget {
+  const WeekComing({ Key? key }) : super(key: key);
+
+  @override
+  _WeekComingState createState() => _WeekComingState();
+}
+
+class _WeekComingState extends State<WeekComing> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+    );
+  }
+}
