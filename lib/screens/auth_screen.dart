9479c62b1adf451cc33c10a87b3fd4fd674f85df
Kevw94 <pinerokevin@icloud.com>
2022-11-16 14:12:54 +0100

    implement form for registering a user

---
+++ lib/screens/auth_screen.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+import '../widgets/signup_form.dart';
+
+class AuthScreen extends StatefulWidget {
+  const AuthScreen({ Key? key }) : super(key: key);
+
+  @override
+  _AuthScreenState createState() => _AuthScreenState();
+}
+
+class _AuthScreenState extends State<AuthScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("test"),
+      ),
+       body: const SignupForm()
+    );
+  }
+}
---
+++ lib/widgets/signup_form.dart
@@ -0,0 +1,126 @@
+import 'package:flutter/material.dart';
+
+import '../models/user.dart';
+import '../providers/mongodb.dart';
+
+// instance of MongoDB
+var mongodb = DBConnection.getInstance();
+
+class SignupForm extends StatefulWidget {
+  const SignupForm({super.key});
+
+  @override
+  SignupFormState createState() {
+    return SignupFormState();
+  }
+}
+
+
+class SignupFormState extends State<SignupForm> {
+  final _formKey = GlobalKey<FormState>();
+  final nameController = TextEditingController();
+  final mailController = TextEditingController();
+  final passwordController = TextEditingController();
+  final imageController = TextEditingController();
+
+  @override
+  void dispose() {
+    // Clean up the controller when the widget is disposed.
+    nameController.dispose();
+    mailController.dispose();
+    passwordController.dispose();
+    imageController.dispose();
+    super.dispose();
+  }
+
+  void _createUser(TextEditingController nameController, TextEditingController mailController, TextEditingController passwordController, TextEditingController imageController) {
+    setState(() {
+      var collection = mongodb.getCollection("users");
+      collection.insertOne({
+        "userName": nameController.text,
+        "email": mailController.text,
+        "password": passwordController.text,
+        "image": imageController.text,
+      });
+    });
+  }
+  @override
+  Widget build(BuildContext context) {
+
+    return Form(
+      key: _formKey,
+      child: Column(
+        children: <Widget>[
+          TextFormField(
+            // The validator receives the text that the user has entered.
+            decoration: const InputDecoration(
+              border: UnderlineInputBorder(),
+              labelText: 'Entrez un nom d\'utilisateur',
+            ),
+            validator: (value) {
+              if (value == null || value.isEmpty) {
+                return 'Merci de renseigner du texte';
+              }
+            },
+            controller: nameController,
+          ),
+          TextFormField(
+            // The validator receives the text that the user has entered.
+            decoration: const InputDecoration(
+              border: UnderlineInputBorder(),
+              labelText: 'Entrez une adresse mail',
+            ),
+            validator: (value) {
+              if (value == null || value.isEmpty) {
+                return 'Merci de renseigner une adresse mail';
+              }
+            },
+            controller: mailController,
+          ),
+          TextFormField(
+            // The validator receives the text that the user has entered.
+            decoration: const InputDecoration(
+              border: UnderlineInputBorder(),
+              labelText: 'Entrez un mot de passe',
+            ),
+            validator: (value) {
+              if (value == null || value.isEmpty) {
+                return 'Merci de renseigner un mot de passe';
+              }
+            },
+            controller: passwordController,
+          ),
+          TextFormField(
+            // The validator receives the text that the user has entered.
+            decoration: const InputDecoration(
+              border: UnderlineInputBorder(),
+              labelText: 'Entrez l\'url d\'une image pour votre photo de profil',
+            ),
+            validator: (value) {
+              if (value == null || value.isEmpty) {
+                return 'Merci de renseigner une url';
+              }
+            },
+            controller: imageController,
+          ),
+          ElevatedButton(
+            onPressed: () {
+              // Validate returns true if the form is valid, or false otherwise.
+              if (_formKey.currentState!.validate()) {
+                // If the form is valid, display a snackbar. In the real world,
+                // you'd often call a server or save the information in a database.
+                ScaffoldMessenger.of(context).showSnackBar(
+                  const SnackBar(content: Text('Processing Data')),
+                );
+                _createUser(nameController, mailController, passwordController, imageController);
+                Navigator.pop(context);
+              }
+            },
+            style: ElevatedButton.styleFrom(shape: const StadiumBorder()),
+            child: const Text('Submit'),
+          ),
+        ],
+      ),
+    );
+  }
+}
